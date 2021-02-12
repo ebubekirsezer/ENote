805701a9c77644df338895edc8d7f4cc315f49cf
Ebubekir Sezer <ebubekirssezer@gmail.com>
2021-02-12 22:33:16 +0300

    Project initially created

---
+++ ENote/ENote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ENote
+//
+//  Created by Ebubekir Sezer on 12.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
