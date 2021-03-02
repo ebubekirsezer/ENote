96a179a8a4a4a3880a5975963e3d7b41a59f4f8a
Ebubekir Sezer <ebubekirssezer@gmail.com>
2021-03-02 22:04:28 +0300

    DEVELOP: Top Buttons arranged

--- ENote/ENote/Controllers/Login/LoginViewController.swift
+++ ENote/ENote/Controllers/Login/LoginViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    
+    @IBOutlet weak var loginView: UIView?
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
     @IBOutlet weak var loginEmailTextField: UITextField?
     @IBOutlet weak var loginEmailIcon: UIImageView?
     @IBOutlet weak var loginPasswordTextField: UITextField?
@@ -41,5 +45,21 @@ class LoginViewController: UIViewController {
                 loginPasswordStackView?.changeStackCustomAppearenceWith(image: loginPasswordIcon)
             }
         }
+    }
+    
+    @IBAction func loginTabButtonPressed(_ sender: UIButton) {
+        loginButton.backgroundColor = BaseColor.PrimaryColor
+        signupButton.backgroundColor = BaseColor.SecondaryColor
+        loginButton.setTitleColor(.white, for: .normal)
+        signupButton.setTitleColor(BaseColor.PrimaryColor, for: .normal)
+        loginView?.isHidden = false
+    }
+    
+    @IBAction func signupTabButtonPressed(_ sender: UIButton) {
+        loginButton.backgroundColor = BaseColor.SecondaryColor
+        signupButton.backgroundColor = BaseColor.PrimaryColor
+        loginButton.setTitleColor(BaseColor.PrimaryColor, for: .normal)
+        signupButton.setTitleColor(.white, for: .normal)
+        loginView?.isHidden = true
     }
 }
