e3132f7d984bdebaecede0b56251d145a1512fff
Ebubekir Sezer <ebubekirssezer@gmail.com>
2021-03-02 21:02:41 +0300

    DEVELOP: IBOutlet connected and textdidchange method added

--- ENote/ENote/Controllers/Login/LoginViewController.swift
+++ ENote/ENote/Controllers/Login/LoginViewController.swift
@@ -10,7 +10,26 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var loginEmailTextField: UITextField?
+    @IBOutlet weak var loginEmailIcon: UIImageView?
+    @IBOutlet weak var loginPasswordTextField: UITextField?
+    @IBOutlet weak var loginPasswordIcon: UIImageView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTarget()
+    }
+    
+    private func setupTarget(){
+        loginEmailTextField?.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        loginPasswordTextField?.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    @objc private func textFieldDidChange(_ textField: UITextField){
+        if textField == loginEmailTextField {
+            print("email")
+        } else if textField == loginPasswordTextField {
+            print("password")
+        }
     }
 }
